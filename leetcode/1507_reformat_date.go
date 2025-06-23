e05878d1c0e8ff2055958c37c812aa00ca4cf527
Ngo Xuan Thiep <ngothiep2412@gmail.com>
2025-06-23 21:08:00 +0700

    leetcode: add 1507_reformat_date

---
+++ leetcode/1507_reformat_date.go
@@ -0,0 +1,36 @@
+package main
+
+func reformatDate(date string) string {
+	result := ""
+
+	monthMap := map[string]string{
+		"Jan": "01",
+		"Feb": "02",
+		"Mar": "03",
+		"Apr": "04",
+		"May": "05",
+		"Jun": "06",
+		"Jul": "07",
+		"Aug": "08",
+		"Sep": "09",
+		"Oct": "10",
+		"Nov": "11",
+		"Dec": "12",
+	}
+
+	if len(date) == 12 {
+		date = "0" + date
+	}
+
+	year := date[8:]
+	month := monthMap[date[5:8]]
+	day := date[0:2]
+	result = year + "-" + month + "-" + day
+
+	return result
+}
+
+func main() {
+	result := reformatDate("20th Oct 2052")
+	print(result)
+}
--- leetcode/1528_shuffle_string.go
+++ leetcode/1528_shuffle_string.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func restoreString(s string, indices []int) string {
 	result := make([]byte, len(s))
 
@@ -11,11 +9,11 @@ func restoreString(s string, indices []int) string {
 	return string(result)
 }
 
-func main() {
-	result := restoreString("codeleet", []int{4, 5, 6, 7, 0, 2, 1, 3})
-
-	fmt.Println(result)
-}
+//func main() {
+//	result := restoreString("codeleet", []int{4, 5, 6, 7, 0, 2, 1, 3})
+//
+//	fmt.Println(result)
+//}
 
 // O(n)
 // O(n)
