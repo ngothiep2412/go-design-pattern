8fb3b6c9010fae1fc1ef0c937f1c65b059b7570d
Ngo Xuan Thiep <ngothiep2412@gmail.com>
2025-06-23 20:53:45 +0700

    leetcode: add 1528_shuffle_string

---
+++ leetcode/1528_shuffle_string.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func restoreString(s string, indices []int) string {
+	result := make([]byte, len(s))
+
+	for i, v := range indices {
+		result[v] = s[i] //result[4] = s[0]
+	}
+	return string(result)
+}
+
+func main() {
+	result := restoreString("codeleet", []int{4, 5, 6, 7, 0, 2, 1, 3})
+
+	fmt.Println(result)
+}
+
+// O(n)
+// O(n)
